03e16599d2fb43c4bb0eb49fba56d46bc1b8b2d6
makennamartin97 <makennamartin97@yahoo.com>
2022-10-13 17:44:04 -0400

    redeploy

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,6 +1,5 @@
-import React,{useState} from "react";
+import React from "react";
 import code from '../assets/usestate.png';
-import ModalState from "./modals/ModalState";
 
 const UseState = () => {
      return(
